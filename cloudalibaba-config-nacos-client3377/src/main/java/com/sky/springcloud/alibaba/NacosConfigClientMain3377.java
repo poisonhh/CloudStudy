d4521406089c2647786b0d4837b24f773bc8a1f3
poisonhh <poison0869@gmail.com>
2020-05-25 15:58:07 +0800

    update

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/sky/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.sky.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author hhuang
+ * @date 2020-05-25 15:50
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/sky/springcloud/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.sky.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hhuang
+ * @date 2020-05-25 15:51
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
